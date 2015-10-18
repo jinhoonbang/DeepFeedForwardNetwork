30bbb0d9c9fe6876783630a4361fe9bb3db65fe0
unknown <jinhoonbang@u.northwestern.edu>
2015-10-18 12:00:51 -0500

    cast y to int 64 in errors

--- src/layersFFN/softMax.py
+++ src/layersFFN/softMax.py
@@ -162,6 +162,7 @@ class SoftMax(object):
         #         ('y', y.type, 'y_pred', self.y_pred.type)
         #     )
         # check if y is of the correct datatype
+        y = T.cast(y, 'int64')
         if y.dtype.startswith('int'):
             # the T.neq operator returns a vector of 0s and 1s, where 1
             # represents a mistake in prediction
